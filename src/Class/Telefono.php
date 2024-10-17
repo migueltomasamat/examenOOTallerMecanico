2bd9d6d501306d252fb48f3ec79d0eedbd8badb1
matomas <migueltomas.informatica@iespacomolla.es>
2024-10-17 18:55:57 +0200

    Clase Teléfono añadida

---
+++ src/Class/Telefono.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Class;
+
+class Telefono
+{
+    private string $prefijo;
+    private string $numero;
+
+    public function __construct(string $prefijo,string$numero)
+    {
+        $this->prefijo=$prefijo;
+        $this->numero=$numero;
+    }
+
+    public function getPrefijo(): string
+    {
+        return $this->prefijo;
+    }
+
+    public function setPrefijo(string $prefijo): void
+    {
+        //TODO almacenar el prefijo siguiendo un estándar +34, +1-349
+        $this->prefijo = $prefijo;
+    }
+
+    public function getNumero(): string
+    {
+        return $this->numero;
+    }
+
+    public function setNumero(string $numero): void
+    {
+        $this->numero = $numero;
+    }
+
+
+    //Espacio para la funciones definidas por el programador
+
+    public static function comprobarPrefijo(string $prefijo):bool{
+
+        //TODO implementar la funcion para comprobar un prefijo de teléfono
+        return false;
+    }
+
+    public function obtenerTelefonoFormateado():string
+    {
+        //TODO implementar la función para unir dos teléfonos según un formato (+34) 657 245 378
+        return "telefono formateado";
+    }
+}
