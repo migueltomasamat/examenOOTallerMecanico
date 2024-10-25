5c093a503fa531e1562aa0ee11c6c655da7d6f96
matomas <migueltomas.informatica@iespacomolla.es>
2024-10-25 18:49:55 +0200

    Añadido logo

--- src/View/template/navegacion.php
+++ src/View/template/navegacion.php
@@ -1,7 +1,9 @@
 <!-- BOTTOM NAV -->
 <nav class="navbar navbar-expand-lg navbar-light bg-white sticky-top">
     <div class="container">
-        <a class="navbar-brand" href="#">Prixima<span class="dot">.</span></a>
+        <a class="navbar-brand" href="#">ReserDAWtions<span class="dot">.</span></a>
+        <!--<a class="navbar-brand" style="width: 8vw" href="/"><img src="<?=DIRECTORIO_RECURSOS?>img/logo.png"</a>-->
+
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -9,7 +11,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="#home">Inicio</a>
+                    <a class="nav-link" href="/">Inicio</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="<?=DIRECTORIO_VISTAS?>about.php">Sobre Nosotros</a>
