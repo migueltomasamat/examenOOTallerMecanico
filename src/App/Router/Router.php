783ee114ec31ecf683492af7340d0d7f1ad61867
miguel <migueltomas.informatica@iespacomolla.es>
2025-01-29 19:41:20 +0100

    Router actualizado para aceptar id's numéricos

--- src/App/Router/Router.php
+++ src/App/Router/Router.php
@@ -51,7 +51,7 @@ class Router
     private function cambiar_id_uri(string $uri):string{
         $uriArray = explode('/',$uri);
         //var_dump($uriArray);
-        if (count($uriArray)>2 && Uuid::isValid($uriArray[2])){
+        if (count($uriArray)>2 && (Uuid::isValid($uriArray[2]) or is_numeric($uriArray[2]))){
             $uriArray[2]="{id}";
         }
         return implode("/",$uriArray);
